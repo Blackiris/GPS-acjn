2bf9d5489c5eba59e6853f9ecb0ac3f933156350
Julien Nguyen <jnguyen@etu.emse.fr>
2013-02-01 16:43:42 +0100

    Petite modification dans le modèle pour préparer les itinéraires

--- ejbModule/fr/emse/clientadmin/DataModel.java
+++ ejbModule/fr/emse/clientadmin/DataModel.java
@@ -1,18 +1,21 @@
 package fr.emse.clientadmin;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import fr.emse.server.AdminBeanRemote;
+import fr.emse.server.Coordinate;
 import fr.emse.server.Itinerary;
 import fr.emse.server.Note;
 
 public class DataModel {
 	private AdminBeanRemote adminBeanRemote;
 	
-	private List<Note> notes;
+	Map<Coordinate, Note> mapNotes;
 	private List<Itinerary> itineraries;
 	
 	public DataModel() throws NamingException {
@@ -21,17 +24,25 @@ public class DataModel {
 		System.out.println("Recherche du bean...");
 		adminBeanRemote = (AdminBeanRemote) ctx.lookup("java:global/GPS-acjn/AdminEJB!fr.emse.server.AdminBeanRemote");
 		
-		notes = adminBeanRemote.getNotes();
+		List<Note> notes = adminBeanRemote.getNotes();
+		for (Note note : notes) {
+			mapNotes.put(note.getCoordinate(), note);
+		}
+		
 		itineraries = adminBeanRemote.getItineraries();
 	}
 
 	public void addNote(Note newNote) {
-		notes.add(newNote);
+		mapNotes.put(newNote.getCoordinate(), newNote);
 		adminBeanRemote.addNote(newNote);
 	}
 	
 	public List<Note> getNotes() {
-		return notes;
+		return new ArrayList<Note>(mapNotes.values());
+	}
+	
+	public Note getNodeAt(double latitude, double longitude) {
+		return mapNotes.get(new Coordinate(latitude, longitude));
 	}
 	
 	public void addItinerary(Itinerary newItinerary) {
